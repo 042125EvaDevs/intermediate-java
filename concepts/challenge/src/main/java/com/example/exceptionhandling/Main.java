438c4504fc5dcf692fbcb02c7c19028fe1f43626
srawat <ShivaRawat@Shivas-MBP.lan>
2025-05-08 12:44:47 -0400

    updatedexceptionhandling

--- concepts/challenge/src/main/java/com/example/exceptionhandling/Main.java
+++ concepts/challenge/src/main/java/com/example/exceptionhandling/Main.java
@@ -7,6 +7,16 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
+        try {
+            String anything = null;
+            anything.length();
+            throw new ArithmeticException("Intentionally thrown exception");
+        }
+        catch(ArithmeticException | NullPointerException e) {
+            System.out.println("Catch block: " + e);
+        } finally {
+            System.out.println("We have reached the finally");
+        }
       /**
        *
        * Develop a file reader application that reads data from a file and processes it.
